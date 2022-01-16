1cbe4830c8066c9055e248af13817c1422dcb0cc
Jean Christophe Gomis <devdujour@yahoo.com>
2022-01-16 21:37:14 +0100

    modification du context

--- src/Pages/DashboardFinance/DashboardFinance.js
+++ src/Pages/DashboardFinance/DashboardFinance.js
@@ -1,31 +1,39 @@
-import React from 'react'
-import { useState, useContext } from 'react'
-import BarChart from '../../Components/AllChart/BarChart'
-import { DashboardContext } from '../../context/DashboardContext'
-import './DashboardFinance.css'
-
+import React, { useState, useContext } from "react";
+import "./DashboardFinance.css";
+import BarChart from "../../Components/AllChart/BarChart";
+import { DashboardContext } from "../../context/DashboardContext";
 
 export default function DashboardFinance() {
+      const { dataChart, changeYear, yearData } = useContext(DashboardContext);
 
-      const { dataChart, changeYear, yearData } = useContext(DashboardContext)
-
+      console.log(dataChart, changeYear, yearData);
 
       return (
             <div className="global-container">
-                  <h1>LES RESULTATS DE L'ANNEE: {yearData}</h1>
+                  <h1>Les résultats de l'année : {yearData}</h1>
 
                   <form>
-                        <label htmlFor='year'>Choississez une date.</label>
-                        <select onChange={changeYear} id='year'>
-                              <option value=" 2021"> 2021</option>
-                              <option value=" 2020"> 2020</option>
-                              <option value=" 2019"> 2019</option>
-                              <option value=" 2018"> 2018</option>
+                        <label htmlFor="year">Choisissez une date</label>
+                        <select onChange={changeYear} id="year">
+                              <option value="2020">
+                                    2020
+                              </option>
+                              <option value="2019">
+                                    2019
+                              </option>
+                              <option value="2018">
+                                    2018
+                              </option>
+                              <option value="2017">
+                                    2017
+                              </option>
                         </select>
                   </form>
+
                   <div className="dashboard-container">
-                        <BarChart data={dataChart.chart1} name={"chiffres bimestriels"} />
+                        <BarChart data={dataChart.chart1} name={"Chiffres bimestriels"} />
+
                   </div>
             </div>
-      )
+      );
 }
--- src/context/DashboardContext.js
+++ src/context/DashboardContext.js
@@ -1,29 +1,24 @@
-import React from 'react'
-import { createContext, useState } from 'react'
+import React, { createContext, useState } from 'react';
 import fulldata from './fulldata'
 
 export const DashboardContext = createContext()
 
 const DashboardContextProvider = props => {
 
-      const [dataChart, setDataChart] = useState(fulldata["2021"])
-
-      const [yearData, setYearData] = useState("2021")
+      const [dataChart, setDataChart] = useState(fulldata['2020'])
+      const [yearData, setYearData] = useState("2020")
 
       const changeYear = el => {
-
             setYearData(el.target.value)
-            // eslint-disable-next-line no-whitespace-before-property
             setDataChart(fulldata[el.target.value])
       }
 
       return (
             <DashboardContext.Provider value={{ changeYear, dataChart, yearData }}>
-
                   {props.children}
-
             </DashboardContext.Provider>
       )
-}
-export default DashboardContextProvider
 
+}
+
+export default DashboardContextProvider;
